42e9d43a46cf5e7b2de1de45e9992d50291eed21
Paul Mesnilgrente <web@paul-mesnilgrente.com>
2021-10-14 20:09:33 +0200

    feat: got the logo up the text on small screens

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -1,52 +1,54 @@
 export default function Contact() {
   return (
-    <section className="container-fluid pb-5" id="contact">
-      <h1>Contact</h1>
+    <section className="pb-5" id="contact">
+      <div className="container">
+        <h1>Contact</h1>
 
-      <div className="row">
-        <div className="col-xs-12 col-md-4">
-          <address className="mb-3">
-            <strong>Dans ma librairie</strong>
-            <br />
-            32 rue Garonne
-            <br />
-            47000 Agen
+        <div className="row">
+          <div className="col-xs-12 col-md-4">
+            <address className="mb-3">
+              <strong>Dans ma librairie</strong>
+              <br />
+              32 rue Garonne
+              <br />
+              47000 Agen
 
-            <br />
-            <abbr title="Téléphone">Tél :</abbr> (+33) 5 53 48 25 68
-          </address>
+              <br />
+              <abbr title="Téléphone">Tél :</abbr> (+33) 5 53 48 25 68
+            </address>
 
-          <address>
-            <strong>Email</strong>
-            <br />
-            <a href="mailto:contact@dansmalibrairie.com">contact@dansmalibrairie.com</a>
-          </address>
+            <address>
+              <strong>Email</strong>
+              <br />
+              <a href="mailto:contact@dansmalibrairie.com">contact@dansmalibrairie.com</a>
+            </address>
 
-          <p className="pt-5">
-            <strong>Horaires d'ouverture</strong>
-            <br />
-            <dl class="row">
-              <dt class="col-sm-3">Lundi</dt>
-              <dd class="col-sm-9">15h00 - 18h45</dd>
+            <p className="pt-5">
+              <strong>Horaires d'ouverture</strong>
+              <br />
+              <dl class="row">
+                <dt class="col-sm-3">Lundi</dt>
+                <dd class="col-sm-9">15h00 - 18h45</dd>
 
-              <dt class="col-sm-3">Mardi au samedi</dt>
-              <dd class="col-sm-9">10h30 - 12h30 / 14h - 18h45</dd>
+                <dt class="col-sm-3">Mardi au samedi</dt>
+                <dd class="col-sm-9">10h30 - 12h30 / 14h - 18h45</dd>
 
-              <dt class="col-sm-3">Dimanche</dt>
-              <dd class="col-sm-9">FERMÉ</dd>
-            </dl>
-          </p>
-        </div>
-        <div className="col-xs-12 col-md-8">
-          <iframe
-            title="Carte pointé avec Dans ma librairie"
-            className="w-100"
-            width="450"
-            height="250"
-            frameborder="0"
-            src="https://www.google.com/maps/embed/v1/place?key=AIzaSyBvQZT8HUhTmBJCiYha7I2n8qT81f6SQeg&q=Dans ma librairie, 32 rue garonne, 47000 Agen"
-            allowfullscreen
-          ></iframe>
+                <dt class="col-sm-3">Dimanche</dt>
+                <dd class="col-sm-9">FERMÉ</dd>
+              </dl>
+            </p>
+          </div>
+          <div className="col-xs-12 col-md-8">
+            <iframe
+              title="Carte pointé avec Dans ma librairie"
+              className="w-100"
+              width="450"
+              height="250"
+              frameborder="0"
+              src="https://www.google.com/maps/embed/v1/place?key=AIzaSyBvQZT8HUhTmBJCiYha7I2n8qT81f6SQeg&q=Dans ma librairie, 32 rue garonne, 47000 Agen"
+              allowfullscreen
+            ></iframe>
+          </div>
         </div>
       </div>
     </section>
--- src/Header.jsx
+++ src/Header.jsx
@@ -1,43 +1,48 @@
 import logo from './logo.png';
 
-import { FaFacebookSquare as FacebookIcon } from 'react-icons/fa';
-import { FaInstagramSquare as InstagramIcon } from 'react-icons/fa';
+import {
+  FaFacebookSquare as FacebookIcon,
+  FaInstagramSquare as InstagramIcon
+} from 'react-icons/fa';
+
 
 export default function Header() {
   return (
-    <header className="container-fluid" id="page-header">
-      <div className="row align-items-center justify-content-center py-3">
-        <div className="col-sm-4 col-xs-12">
-          <div className="row">
-            <h1 className="col-12 display-5 fw-bold">
-              Retrouvez-nous !
-            </h1>
-            <p className="lead">
-              Vous nous trouverez sur place à Agen ou sur les réseaux sociaux pour commander
-              vos livres.
-            </p>
-            <div className="col">
-              <a
-                href="https://www.facebook.com/Dans-ma-librairie-297290237076305"
-                target="_blank"
-                rel="noopener noreferrer"
-                className="social-network me-3"
-              >
-                <FacebookIcon size="4rem" />
-              </a>
-              <a
-                href="https://www.instagram.com/dans_ma_librairie/"
-                target="_blank"
-                rel="noopener noreferrer"
-                className="social-network"
-              >
-                <InstagramIcon size="4rem" />
-              </a>
+    <header id="page-header" className="header-background">
+      <div className="container">
+        <div className="row align-items-center justify-content-center py-3">
+          <div className="col-md-8 order-md-2 text-center pb-3">
+            <img src={logo} alt="logo" />
+          </div>
+          <div className="col-md-4 order-md-1">
+            <div className="row">
+              <h1 className="col-12 display-5 fw-bold">
+                Retrouvez-nous !
+              </h1>
+              <p className="lead">
+                Vous nous trouverez sur place à Agen ou sur les réseaux sociaux pour commander
+                vos livres.
+              </p>
+              <div className="col">
+                <a
+                  href="https://www.facebook.com/Dans-ma-librairie-297290237076305"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="social-network me-3"
+                >
+                  <FacebookIcon size="4rem" />
+                </a>
+                <a
+                  href="https://www.instagram.com/dans_ma_librairie/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="social-network"
+                >
+                  <InstagramIcon size="4rem" />
+                </a>
+              </div>
             </div>
           </div>
-        </div>
-        <div className="col-sm-8 col-xs-12 text-center">
-          <img src={logo} alt="logo" />
         </div>
       </div>
     </header>
